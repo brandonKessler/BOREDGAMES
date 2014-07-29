953610c94dcfb72f84fe3b292ac905f97c117d4c
KristenWise <kristenwise13@dyn-160-39-135-148.dyn.columbia.edu>
2014-07-29 16:58:30 -0400

    Added ast file
    First part of it; trying to get type signatures figured out.
    Program returns a list of all variables, including Players, and list of
    Pieces.

---
+++ ast.ml
@@ -0,0 +1,33 @@
+
+type op = Add | Sub | Mult | Div | Equal | Neq | Less | Leq | Greater | Geq
+
+type expr = 
+   Literal of int
+ | Id of string
+ | Binop of expr * op * expr
+ | Assign of string * expr
+ | Call of string * expr list
+ | Noexpr
+
+type stmt = 
+   Block of stmt list
+ | Expr of expr
+ | Return of expr
+ | If of expr * stmt * stmt
+ | Loop of expr * stmt
+
+type piece_t = { 
+   owner : string;
+   name : string;
+   num : int;
+   ptval : int;
+ }
+
+type board_t = {
+   rows : int;
+   cols : int;
+ }
+
+
+type program = string list * piece_t list
+
