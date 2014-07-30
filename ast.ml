0915a2025a518cd9fad6be082a71f7bfb231b218
KristenWise <kristenwise13@dyn-160-39-135-148.dyn.columbia.edu>
2014-07-30 17:18:21 -0400

    Edited ast
    Added section for types

--- ast.ml
+++ ast.ml
@@ -2,7 +2,11 @@
 type op = Add | Sub | Mult | Div | Equal | Neq | Less | Leq | Greater | Geq
 
 type expr = 
-   Literal of int
+   Lint of int
+ | Ldouble of float
+ | Lbool of bool
+ | Lcoord of coord_t
+ | Lstring of string
  | Id of string
  | Binop of expr * op * expr
  | Assign of string * expr
